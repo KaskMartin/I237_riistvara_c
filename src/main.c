b947b019c18633cc150276f8d42307758b8a2b70
Martin Kask <kask.martin@gmail.com>
2016-11-20 16:33:59 +0200

    LCD printimise lisamine

--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@
 #include "print_helper.h"
 #include "hmi_msg.h"
 #include <avr/pgmspace.h>
+#include "../lib/hd44780_111/hd44780.h"
 
 #define BLINK_DELAY_MS 100
 
@@ -19,6 +20,8 @@ void main (void)
     /* Init error console as stderr in UART3 and print user code info */
     uart0_initialize();
     uart3_initialize();
+    lcd_init();
+    lcd_clrscr();
     stdin = stdout = &uart0_io;
     stderr = &uart3_out;
     fprintf_P(stderr, PSTR(VER_FW),
@@ -26,8 +29,9 @@ void main (void)
     fprintf_P(stderr, PSTR(VER_LIBC), __AVR_LIBC_VERSION_STRING__);
     /* End UART3 init and info print */
     
-    /*Print my name*/
+    /*Print my name to stdout and LCD*/
     fprintf_P(stdout, PSTR(STUD_NAME "\n"));
+    lcd_puts_P(PSTR(STUD_NAME));
     /* Print ASCII Table */
     print_ascii_tbl(stdout);
     unsigned char ascii[128] = {0};
@@ -47,11 +51,19 @@ void main (void)
         fprintf_P(stdout, PSTR(MONTH_PROMPT));
         fscanf(stdin, "%c", &inBuf);
         fprintf(stdout, "%c\n", inBuf);
+        /*Start printing Months to LCD on second line */
+        lcd_goto(0x40);
 
+        /*Loop to find and print Month names to stdout and LCD*/
         for (int i = 0; i < 6; i++) {
             if (!strncmp_P(&inBuf, (PGM_P)pgm_read_word(&nameMonth[i]), 1)) {
                 fprintf_P(stdout, PSTR("%S\n"), (PGM_P)pgm_read_word(&nameMonth[i]));
+                lcd_puts_P((PGM_P)pgm_read_word(&nameMonth[i]));
+                lcd_putc(' ');
             }
+        }
+        for (int i = 1; i<=16; i++) {
+            lcd_putc(' ');
         }
 
         /* Set pin 3 low to turn LED off */
